c36c032c423f487f29183ceaf5973d26489a2b3b
IAmVengeance <ayan.saxena24@gmail.com>
2024-01-15 15:05:32 +0530

    test

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import React, { Component } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  increment = () =>
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1,
+      }
+    })
+
+    decrement = () =>
+    this.setState(prevState => {
+      return {
+        count: prevState.count - 1,
+      }
+    })
+
+    render() {
+      return (
+        <div className='card'>
+          <button onClick={this.increment}>+</button>
+          <p>{this.state.count}</p>
+          <button onClick={this.decrement}>-</button>
+        </div>
+      )
+    }
+}
+
+export default App
---
+++ src/App.test.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
+
+describe("App", () => {
+  it("starts with a count of 0", () => {
+    const wrapper = shallow(<App />);
+    const countState = wrapper.state().count;
+    expect(countState).toEqual(0);
+  });
+  // it('increments count by 1 when the increment button is clicked', () => {
+  //     const wrapper = shallow(<App />)
+  //     const incrementBtn = wrapper.find('button.increment')
+  //     incrementBtn.simulate('click')
+  //     const text = wrapper.find('p').text()
+  //     expect(text).toEqual('1')
+  // })
+  // it('decrements count by 1 when the decrement button is clicked', () => {
+  //     const wrapper = shallow(<App />)
+  //     const decrementBtn = wrapper.find('button.decrement')
+  //     decrementBtn.simulate('click')
+  //     const text = wrapper.find('p').text()
+  //     expect(text).toEqual('-1')
+  // })
+});
