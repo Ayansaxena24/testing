83734c1d36c7b2fadc93d9ae6c943c1a2b5fbf56
akarsh@metafic.co <akarsh@metafic.co>
2024-01-15 15:42:58 +0530

    fix testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import React, { Component } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 class App extends Component {
   state = {
